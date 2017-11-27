ecc3ed685adadba4b094f1eb378b10821d3729e1
Peter Decuyper <zero2one@serial-graphics.be>
2017-11-27 11:53:14 +0100

    DMOH-3: Added the DateTime value object.

---
+++ tests/Value/DateTimeTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace StadGent\Services\Test\OpeningHours\Value;
+
+use StadGent\Services\OpeningHours\Value\DateTime;
+use StadGent\Services\OpeningHours\Value\ValueInterface;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Tests for the Date object.
+ */
+class DateTimeTest extends TestCase
+{
+    /**
+     * Test creation an Date from array data.
+     */
+    public function testConstructor()
+    {
+        $string = '2017-03-29T12:18:00+01:00';
+        $dateTime = new DateTime('2017-03-29T12:18:00+01:00');
+        $this->assertEquals($string, (string) $dateTime);
+    }
+
+    /**
+     * Test SameValueAs method.
+     */
+    public function testSameValueAs()
+    {
+        $string = '2017-03-29T12:18:00+01:00';
+        $dateTime = new DateTime('2017-03-29T12:18:00+01:00');
+        $notDateTime = $this
+            ->getMockBuilder(ValueInterface::class)
+            ->getMock();
+        /* @var $notDateTime ValueInterface */
+        $this->assertFalse(
+            $dateTime->sameValueAs($notDateTime),
+            'Compared value object is not a DateTime object.'
+        );
+
+        $dateTimeNotSame = new DateTime('2017-03-30T12:34:56+01:00');
+        $this->assertFalse(
+            $dateTime->sameValueAs($dateTimeNotSame),
+            'Sources do not share the same values.'
+        );
+
+        $dateTimeSame = new DateTime($string);
+        $this->assertTrue(
+            $dateTime->sameValueAs($dateTimeSame),
+            'Sources share the same values.'
+        );
+    }
+
+    /**
+     * Test toString method.
+     */
+    public function testToString()
+    {
+        $string = '2017-03-29T12:18:00+01:00';
+
+        $dateTime = new DateTime($string);
+        $this->assertEquals(
+            $string,
+            (string) $dateTime,
+            'String version of date = date in format "DATE_W3C"'
+        );
+    }
+}
