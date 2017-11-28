fc53c0e34f821175012df3fa6b8273be61e9011e
Peter Decuyper <zero2one@serial-graphics.be>
2017-11-28 13:56:46 +0100

    DMOH-11: Added the OpeningHoursCollection value object.

---
+++ src/Value/OpeningHoursCollection.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace StadGent\Services\OpeningHours\Value;
+
+/**
+ * Object describing a collection of OpeningHours.
+ *
+ * @package StadGent\Services\OpeningHours\Value
+ */
+class OpeningHoursCollection extends CollectionAbstract implements ValueFromArrayInterface
+{
+    /**
+     * Use only the named constructors.
+     */
+    protected function __construct()
+    {
+        // The constructor is protected:
+        // Create the object using the named constructors.
+    }
+
+    /**
+     * Create a Collection of OpeningHours objects from an array of data.
+     *
+     * The array may contain a set of OpeningHours array data.
+     *
+     * @returns \StadGent\Services\OpeningHours\Value\OpeningHoursCollection
+     *
+     * @throws \InvalidArgumentException
+     *   If one of the items does not have a date.
+     */
+    public static function fromArray(array $data)
+    {
+        $collection = new static();
+
+        foreach ($data as $key => $item) {
+            $collection->values[$key] = OpeningHours::fromArray($item);
+        }
+
+        return $collection;
+    }
+
+    /**
+     * @inheritdoc
+     *
+     * Will return the channel names separated by ", ":
+     * General, First line, Second line
+     */
+    public function __toString()
+    {
+        $labels = [];
+        foreach ($this->values as $value) {
+            /* @var $value \StadGent\Services\OpeningHours\Value\OpeningHours */
+            $labels[] = (string) $value;
+        }
+
+        return (string) implode(', ', $labels);
+    }
+}
---
+++ tests/Value/OpeningHoursCollectionTest.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace StadGent\Services\Test\OpeningHours\Value;
+
+use StadGent\Services\OpeningHours\Value\OpeningHoursCollection;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Tests for the DaysCollection value object.
+ *
+ * @package Gent\Zalenzoeker\Tests\Value
+ */
+class OpeningHoursCollectionTest extends TestCase
+{
+    /**
+     * Test with empty array.
+     */
+    public function testFromEmptyArray()
+    {
+        $collection = OpeningHoursCollection::fromArray([]);
+        $this->assertEquals(0, $collection->getIterator()->count());
+    }
+
+    /**
+     * Test with data in the array.
+     */
+    public function testFromArray()
+    {
+        $data = [
+            [
+                'channel' => 'FooBar',
+                'channelId' => 1,
+                'openingHours' => [],
+            ],
+            [
+                'channel' => 'FizBuz',
+                'channelId' => 5,
+                'openingHours' => [],
+            ],
+        ];
+        $collection = OpeningHoursCollection::fromArray($data);
+        $this->assertEquals(2, $collection->getIterator()->count());
+    }
+
+    /**
+     * Test comparing 2 equal collections.
+     */
+    public function testSameValueAs()
+    {
+        $data = [
+            [
+                'channel' => 'FooBar',
+                'channelId' => 1,
+                'openingHours' => [],
+            ],
+            [
+                'channel' => 'FizBuz',
+                'channelId' => 5,
+                'openingHours' => [],
+            ],
+        ];
+
+        $collection = OpeningHoursCollection::fromArray($data);
+        $sameCollection = OpeningHoursCollection::fromArray($data);
+
+        $this->assertTrue(
+            $collection->sameValueAs($sameCollection),
+            'Collections are the same.'
+        );
+    }
+
+    /**
+     * Test the method to get a string representing a collection.
+     */
+    public function testToString()
+    {
+        $data = [
+            [
+                'channel' => 'FooBar',
+                'channelId' => 1,
+                'openingHours' => [],
+            ],
+            [
+                'channel' => 'FizzBuzz',
+                'channelId' => 5,
+                'openingHours' => [],
+            ],
+        ];
+
+        $collection = OpeningHoursCollection::fromArray($data);
+        $this->assertEquals(
+            'FooBar, FizzBuzz',
+            (string) $collection,
+            'String version contains channel labels separated by ", ".'
+        );
+    }
+}
