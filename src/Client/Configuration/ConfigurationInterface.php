8426e5287acd730f5b1fccc39672b93aed6181cb
Peter Decuyper <zero2one@serial-graphics.be>
2017-11-29 15:05:07 +0100

    DMOH-7: Added the client configuration object.

---
+++ src/Client/Configuration/ConfigurationInterface.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace StadGent\Services\OpeningHours\Client\Configuration;
+
+/**
+ * Configuration Interface.
+ *
+ * @package StadGent\Services\OpeningHours\Client\Configuration
+ */
+interface ConfigurationInterface
+{
+    /**
+     * ConfigurationInterface constructor.
+     *
+     * @param string $endPointUri
+     *   The endpoint URI.
+     * @param array $options
+     *   Optional options about the service.
+     */
+    public function __construct($endPointUri, array $options = []);
+
+    /**
+     * Get the endpoint URI.
+     *
+     * @return string
+     */
+    public function getUri();
+
+    /**
+     * Get the timeout when calling the endpoint.
+     *
+     * @return int
+     *   Timeout in seconds.
+     */
+    public function getTimeout();
+}
