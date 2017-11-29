627044d9f9dd68dbad02172f9fa9b7eb1628827e
Peter Decuyper <zero2one@serial-graphics.be>
2017-11-29 09:16:08 +0100

    DMOH-7: Added the GetAll Services request.

---
+++ src/Request/RequestAbstract.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace StadGent\Services\OpeningHours\Request;
+
+use GuzzleHttp\Psr7\Request;
+
+/**
+ * Interface MailingList Request
+ *
+ * @package Gent\Services\MailingList\Request
+ */
+abstract class RequestAbstract extends Request implements RequestInterface
+{
+
+}
---
+++ src/Request/RequestInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace StadGent\Services\OpeningHours\Request;
+
+/**
+ * Interface RequestInterface.
+ *
+ * @package StadGent\Services\OpeningHours\Request
+ */
+interface RequestInterface extends \Psr\Http\Message\RequestInterface
+{
+}
