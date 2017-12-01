e88a985b683a5023d6b38ec8336f7a1f07744e15
Peter Decuyper <zero2one@serial-graphics.be>
2017-12-01 14:42:31 +0100

    DMOH-7: Added SearchByLabel example.

--- examples/config.example.php
+++ examples/config.example.php
@@ -7,3 +7,27 @@
 // The API endpoint URL without the /service... part.
 $apiEndpoint = '';
 
+
+// Service Data to lookup data for --------------------------------------------
+
+// Service label to search by.
+$service_label = '';
+
+// Service to load by its id.
+$service_id = '';
+
+
+
+
+
+// Channel Data to lookup data for --------------------------------------------
+
+// Channel to load by its id.
+$channel_id = '';
+
+
+
+
+
+// Dates to lookup OpeningHours for -------------------------------------------
+
