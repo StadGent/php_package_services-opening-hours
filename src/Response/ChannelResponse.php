e3eda42ade65a870ed06b7e14e8b36d6b6148616
Peter Decuyper <zero2one@serial-graphics.be>
2017-12-04 08:55:30 +0100

    DMOH-8: Added the ChannelResponse object.

---
+++ src/Response/ChannelResponse.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace StadGent\Services\OpeningHours\Response;
+
+use StadGent\Services\OpeningHours\Value\Channel;
+
+/**
+ * Response object containing a single Channel.
+ *
+ * @package StadGent\Services\OpeningHours\Response
+ */
+class ChannelResponse implements ResponseInterface
+{
+    /**
+     * The Channel in the response.
+     *
+     * @var \StadGent\Services\OpeningHours\Value\Channel
+     */
+    private $channel;
+
+    /**
+     * ChannelResponse constructor.
+     *
+     * @param \StadGent\Services\OpeningHours\Value\Channel $channel
+     */
+    public function __construct(Channel $channel)
+    {
+        $this->channel = $channel;
+    }
+
+    /**
+     * Get the Channel.
+     *
+     * @return \StadGent\Services\OpeningHours\Value\Channel
+     */
+    public function getChannel()
+    {
+        return $this->channel;
+    }
+}
