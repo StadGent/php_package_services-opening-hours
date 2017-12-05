ffff22eb24169b6e0369190d2070614670a0624b
Peter Decuyper <zero2one@serial-graphics.be>
2017-12-05 12:51:29 +0100

    DMOH-12: Added the HTML response object.

---
+++ src/Response/HtmlResponse.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace StadGent\Services\OpeningHours\Response;
+
+/**
+ * Object containing the HTML response from the API.
+ *
+ * @package StadGent\Services\OpeningHours\Response
+ */
+class HtmlResponse implements ResponseInterface
+{
+    /**
+     * The HTML in the response.
+     *
+     * @var string
+     */
+    private $html;
+
+    /**
+     * HtmlResponse constructor.
+     *
+     * @param string $html
+     */
+    public function __construct($html)
+    {
+        $this->html = $html;
+    }
+
+    /**
+     * Get the HTML string.
+     *
+     * @return string
+     *   The HTML string.
+     */
+    public function getHtml()
+    {
+        return $this->html;
+    }
+}
