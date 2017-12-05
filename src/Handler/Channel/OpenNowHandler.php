a94259744e5301926b0c64d2b7e59b3ee6816760
Peter Decuyper <zero2one@serial-graphics.be>
2017-12-05 15:42:43 +0100

    DMOH-12: Added the OpenNow handlers.

---
+++ src/Handler/Channel/OpenNowHandler.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace StadGent\Services\OpeningHours\Handler\Channel;
+
+use StadGent\Services\OpeningHours\Handler\HandlerAbstract;
+use StadGent\Services\OpeningHours\Request\Channel\OpenNowRequest;
+use StadGent\Services\OpeningHours\Response\OpenNowResponse;
+use Psr\Http\Message as Psr;
+use StadGent\Services\OpeningHours\Value\OpenNow;
+
+/**
+ * Handler to extract the OpenNow data from the response.
+ *
+ * @package StadGent\Services\OpeningHours\Handler\Channel
+ */
+class OpenNowHandler extends HandlerAbstract
+{
+    /**
+     * @inheritDoc
+     */
+    public function handles()
+    {
+        return OpenNowRequest::class;
+    }
+
+    /**
+     * @inheritDoc
+     *
+     * @throws \InvalidArgumentException
+     */
+    public function toResponse(Psr\ResponseInterface $response)
+    {
+        $data = $this->getBodyData($response);
+        $openNow = OpenNow::fromArray($data);
+        return new OpenNowResponse($openNow);
+    }
+}
---
+++ src/Handler/HtmlHandlerAbstract.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace StadGent\Services\OpeningHours\Handler;
+
+use Psr\Http\Message as Psr;
+use StadGent\Services\OpeningHours\Response\HtmlResponse;
+
+/**
+ * Abstract base Handler.
+ *
+ * @package StadGent\Services\OpeningHours\Handler
+ */
+abstract class HtmlHandlerAbstract implements HandlerInterface
+{
+    /**
+     * Get the HTML response from the response object.
+     *
+     * @throws \InvalidArgumentException
+     */
+    public function toResponse(Psr\ResponseInterface $response)
+    {
+        $data = $this->getBodyData($response);
+        return new HtmlResponse($data);
+    }
+
+    /**
+     * Get the array version of the response body.
+     *
+     * @param \Psr\Http\Message\ResponseInterface $response
+     *
+     * @return string
+     */
+    protected function getBodyData(Psr\ResponseInterface $response)
+    {
+        return (string) $response->getBody();
+    }
+}
