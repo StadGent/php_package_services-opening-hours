ccdf77e4646cef55fff0f336d82e39965a364eda
Peter Decuyper <zero2one@serial-graphics.be>
2017-12-06 22:28:07 +0100

    DMOH-15: Added the OpeningHoursMonthHtmlRequest.

---
+++ tests/Request/Channel/OpeningHoursMonthHtmlRequestTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace StadGent\Services\Test\OpeningHours\Request\Channel;
+
+use StadGent\Services\OpeningHours\Request\AcceptType;
+use StadGent\Services\OpeningHours\Request\Channel\OpeningHoursMonthHtmlRequest;
+use StadGent\Services\OpeningHours\Request\MethodType;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Test the OpeningHoursMonthHtmlRequest object.
+ *
+ * @package StadGent\Services\Test\OpeningHours\Request\Service
+ */
+class OpeningHoursMonthHtmlRequestTest extends TestCase
+{
+    /**
+     * Test if the method is GET.
+     */
+    public function testMethodIsGet()
+    {
+        $request = new OpeningHoursMonthHtmlRequest(1, 123, '2020-01-02');
+        $this->assertEquals(MethodType::GET, $request->getMethod());
+    }
+
+    /**
+     * Test if the proper endpoint (URI) is set.
+     */
+    public function testEndpoint()
+    {
+        $request = new OpeningHoursMonthHtmlRequest(1, 123, '2020-01-02');
+        $this->assertEquals(
+            'services/1/channels/123/openinghours/month?date=2020-01-02',
+            $request->getRequestTarget()
+        );
+    }
+
+    /**
+     * Test if the proper headers are set.
+     */
+    public function testHeaders()
+    {
+        $request = new OpeningHoursMonthHtmlRequest(1, 123, '2020-01-02');
+        $this->assertEquals(AcceptType::HTML, $request->getHeaderLine('Accept'));
+    }
+}
