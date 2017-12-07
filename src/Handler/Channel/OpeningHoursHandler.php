14ea61f9f8e7601f8ad332c416a61e3b9baeb9d3
Peter Decuyper <zero2one@serial-graphics.be>
2017-12-07 11:46:02 +0100

    DMOH-10: Removed the obsolete need of an abstract handler.

--- src/Handler/Channel/OpeningHoursHandler.php
+++ src/Handler/Channel/OpeningHoursHandler.php
@@ -2,18 +2,22 @@
 
 namespace StadGent\Services\OpeningHours\Handler\Channel;
 
+use Psr\Http\Message as Psr;
+use StadGent\Services\OpeningHours\Handler\HandlerAbstract;
+use StadGent\Services\OpeningHours\Response\OpeningHoursResponse;
 use StadGent\Services\OpeningHours\Request\Channel\OpeningHoursDayRequest;
 use StadGent\Services\OpeningHours\Request\Channel\OpeningHoursMonthRequest;
 use StadGent\Services\OpeningHours\Request\Channel\OpeningHoursPeriodRequest;
 use StadGent\Services\OpeningHours\Request\Channel\OpeningHoursWeekRequest;
 use StadGent\Services\OpeningHours\Request\Channel\OpeningHoursYearRequest;
+use StadGent\Services\OpeningHours\Value\OpeningHours;
 
 /**
  * Handler to extract the OpeningHours data from the response.
  *
  * @package StadGent\Services\OpeningHours\Handler\Channel
  */
-class OpeningHoursHandler extends OpeningHoursAbstractHandler
+class OpeningHoursHandler extends HandlerAbstract
 {
     /**
      * @inheritDoc
@@ -27,5 +31,17 @@ class OpeningHoursHandler extends OpeningHoursAbstractHandler
             OpeningHoursYearRequest::class,
             OpeningHoursPeriodRequest::class,
         ];
+    }
+
+    /**
+     * @inheritDoc
+     *
+     * @throws \InvalidArgumentException
+     */
+    public function toResponse(Psr\ResponseInterface $response)
+    {
+        $data = $this->getBodyData($response);
+        $openingHours = OpeningHours::fromArray($data);
+        return new OpeningHoursResponse($openingHours);
     }
 }
